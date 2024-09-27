e016fb5fce5711666ff18c64e3dd2cfba24e6de6
Hardik Modha <hardik.modha27@gmail.com>
2024-09-27 12:10:17 +0530

    Initial Commit

---
+++ StudyCI/StudyCIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyCIApp.swift
+//  StudyCI
+//
+//  Created by Hardik Modha on 27/09/24.
+//
+
+import SwiftUI
+
+@main
+struct StudyCIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
